5a3a8fb00efb63a7df77fae0c3f20c7d4100e649
Gor Martsen <Gor.martsen@gmail.com>
2017-04-27 20:08:43 -0400

    - added signature support for UDP messages - added preSendMessage callback - prototyped auth scope - register service as ws on router to receive all broadcast messages.

--- includes/ws.js
+++ includes/ws.js
@@ -7,6 +7,7 @@
 const WebSocket = require('ws');
 const debugF = require('debug');
 const dgram = require('dgram');
+const signature = require('./signature.js');
 
 /**
  * Constructor.
@@ -33,25 +34,7 @@ function WebSocketServer(data) {
   });
 
   self.UDPServer.on('message', function(message, rinfo) {
-    self.debug.debug('UDP: received %s from %s:%s', message, rinfo.address, rinfo.port);
-    var answer = {}
-    try {
-      message = JSON.parse(message);
-    } catch(e) {
-      self.debug.debug('UDP: message parse error %O', e);
-      answer.error = e.message;
-      answer = JSON.stringify(answer);
-
-      return self.UDPServer.send(Buffer.from(answer), rinfo.port, rinfo.address ,function(err) {
-        self.debug.debug('UDP: sent error %O', err);
-      });
-    }
-    process.send(JSON.stringify(message));
-    answer = { message: message };
-    answer = JSON.stringify(answer);
-    return self.UDPServer.send(Buffer.from(answer), rinfo.port, rinfo.address ,function(err) {
-        self.debug.debug('UDP: sent error %O', err);
-      });
+    self.processUDPmessage(message, rinfo);
   });
 
   self.UDPServer.on('listening', function() {
@@ -60,21 +43,97 @@ function WebSocketServer(data) {
   });
 
   process.on('message', function(message) {
-    self.message(message);
+    self.processIPMMessage(message);
   });
+}
+
+/**
+ * Process UDP message.
+ */
+WebSocketServer.prototype.processUDPmessage = function(message, rinfo) {
+  var self = this;
+
+  self.debug.debug('UDP: received %s from %s:%s', message, rinfo.address, rinfo.port);
+  var answer = {}
+  try {
+    message = JSON.parse(message);
+  } catch(e) {
+    self.debug.debug('UDP: message parse error %O', e);
+    answer.error = e.message;
+    answer = JSON.stringify(answer);
+
+    return self.UDPServer.send(Buffer.from(answer), rinfo.port, rinfo.address ,function(err) {
+      if(err) {
+        self.debug.debug('UDP: sent error %O', err);
+      }
+    });
+  }
+  var sign = message.signature;
+  delete message.signature;
+  if(!sign) {
+    answer.error = 'No signature provided';
+    answer = JSON.stringify(answer);
+    return self.UDPServer.send(Buffer.from(answer), rinfo.port, rinfo.address ,function(err) {
+      if(err) {
+        self.debug.debug('UDP: sent error %O', err);
+      }
+    });
+  }
+  if(sign.length != 2) {
+    self.debug.debug('UDP:Signature Malformed signature');
+    answer.error = 'Malformed signature.';
+    answer = JSON.stringify(answer);
+    return self.UDPServer.send(Buffer.from(answer), rinfo.port, rinfo.address ,function(err) {
+      if(err) {
+        self.debug.debug('UDP: sent error %O', err);
+      }
+    });
+  }
+
+  if(sign[1] != signature(sign[0], JSON.stringify(message), process.env.SECURE_KEY)) {
+    answer.error = 'Signature mismatch.';
+    self.debug.debug('UDP:Signature mismatch');
+    answer = JSON.stringify(answer);
+    return self.UDPServer.send(Buffer.from(answer), rinfo.port, rinfo.address ,function(err) {
+      if(err) {
+        self.debug.debug('UDP: sent error %O', err);
+      }
+    });
+  }
+
+  // Send to parent to broadcast it.
+  process.send(JSON.stringify(message));
+  answer = { message: 'Received.' };
+  answer = JSON.stringify(answer);
+
+  return self.UDPServer.send(Buffer.from(answer), rinfo.port, rinfo.address ,function(err) {
+    if(err) {
+      self.debug.debug('UDP: sent error %O', err);
+    }
+  });
+
 }
 
 /**
  * Process IPM process.message request.
  */
-WebSocketServer.prototype.message = function(message) {
+WebSocketServer.prototype.processIPMMessage = function(message) {
   var self = this;
   self.debug.debug('IPM Message received: %s', message.toString());
+  try {
+    message = JSON.parse(message);
+  } catch(e) {
+    return self.debug.debug('JSON parse failed: %s', message.toString());
+  }
 
   self.WSServer.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
-      self.debug.debug('Send message to client');
-      client.send(message);
+      if (self.data.callbacks['preSendMessage']) {
+        self.debug.debug('Send message to client');
+        self.data.callbacks['preSendMessage'](message, client.authData, function(err, answer) {
+          client.send(JSON.stringify(answer));
+        });
+      }
     }
   });
 }
@@ -85,6 +144,9 @@ WebSocketServer.prototype.message = function(message) {
 WebSocketServer.prototype.connection = function(ws) {
   var self = this;
   self.debug.request('Client connected');
+  ws.authData = {
+    test: 0
+  }
   ws.on('message', function(message) {
     self.debug.request('Message received %O', message);
     try {
@@ -94,8 +156,8 @@ WebSocketServer.prototype.connection = function(ws) {
         err: e.message
       });
     }
-    if (self.data.callbacks['clientMessage']) {
-      self.data.callbacks['clientMessage'](message, function(err, answer) {
+    if (self.data.callbacks['receivedMessage']) {
+      self.data.callbacks['receivedMessage'](message, function(err, answer) {
         ws.send(JSON.stringify(answer , null, 2));
       });
     }
--- websocket.js
+++ websocket.js
@@ -4,6 +4,7 @@
 'use strict';
 
 const WSCluster = require('./includes/cluster.js');
+const MicroserviceRouterRegister = require('@microservice-framework/microservice-router-register').register;
 
 const debugF = require('debug');
 
@@ -22,11 +23,46 @@ var mControlCluster = new WSCluster({
     hostname: process.env.HOSTNAME
   },
   callbacks: {
-    clientMessage: websocketClientMessage
+    init: websocketInit,
+    receivedMessage: websocketReceivedMessage,
+    preSendMessage: websocketPreSendMessage
   }
 });
 
-function websocketClientMessage(jsonData, callback) {
-  debug.debug('Client Message %O', jsonData);
+/**
+ * Init Handler.
+ */
+function websocketInit(cluster, worker, address) {
+  if (worker.id == 1 && address.addressType == 4) {
+    console.log('Start register');
+//    console.log(worker);
+    console.log(address);
+    var mserviceRegister = new MicroserviceRouterRegister({
+      server: {
+        url: process.env.ROUTER_URL,
+        secureKey: process.env.ROUTER_SECRET,
+        period: process.env.ROUTER_PERIOD,
+      },
+      route: {
+        path: [process.env.SELF_PATH],
+        url: process.env.SELF_URL,
+        secureKey: process.env.SECURE_KEY,
+        scope: 'ws',
+      },
+      cluster: cluster
+    });
+  }
+}
+
+function websocketPreSendMessage(jsonData, auth, callback) {
+  debug.debug('Received pre Send Message %O %O', auth, jsonData);
+  callback(null, jsonData);
+}
+/**
+ * WS Message handler.
+ * Process message from Client.
+ */
+function websocketReceivedMessage(jsonData, callback) {
+  debug.debug('Received Client Message %O', jsonData);
   callback(null, { answer: 'All GooD', data: jsonData.date});
 }
